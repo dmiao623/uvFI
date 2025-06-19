ca223bcf1f351e0be1839003cb919dcb7d914523
Dustin Miao <Dustin.Miao@jax.org>
2025-06-19 08:42:58 -0400

    added experiment 2025-06-18_kpms-video-analysis

---
+++ experiments/2025-06-18_kpms-video-analysis/src/__init__.py
@@ -0,0 +1,15 @@
+"""Unsupervised Behavior Analysis with KeyPoint-MoSeq.
+
+A research toolkit for analyzing animal behavior using KeyPoint-MoSeq (KPMS) 
+with JABS pose estimation format.
+
+Usage:
+    Simply import the individual modules you need in your Jupyter notebook:
+    
+    from src.methods import load_and_format_data, perform_pca
+    from src.utils import load_keypoints_pd
+    from src.preprocessing import h5_to_csv_poses
+"""
+
+__version__ = "0.1.0"
+__description__ = "Research toolkit for KeyPoint-MoSeq behavioral analysis with JABS format"
